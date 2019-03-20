52f961d4835521da56e8dd6a92cdc39341a72dc3
Arianna Choza <unachoza@gmail.com>
2019-03-20 17:02:23 -0400

    create action creators and reducers functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { selectSong } from "../actions";
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            App
+        </div>
+    )
 }
 
 export default App
---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+
+const songsReducer = () => {
+    return [
+        {
+            title: 'No Scrubs',
+            duration: "4: 05"
+        },
+        {
+          title: 'Macarena',
+          duration: "4: 45"
+        },
+        {
+          title: 'I want it that Way',
+          duration: "4: 15"
+        }
+    ]
+}
+
+const selectedSongsReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    } 
+    return selectedSong
+}
