36e3219b6a3ff79c173d389dfb278b26bb9e2031
Arianna Choza <unachoza@gmail.com>
2019-03-21 13:10:21 -0400

    created SongDetail Componenet connected to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { selectSong } from "../actions";
 import Songlist from './SongList';
+import SongDetail from './SongDetail'
 
 const App = () => {
     return (
@@ -8,6 +9,9 @@ const App = () => {
             <div className="ui row">
                 <div className="column eight wide">
                     <Songlist />
+                </div>
+                 <div className = "column eight wide" >
+                   <SongDetail />
                 </div>
             </div>
         </div>
