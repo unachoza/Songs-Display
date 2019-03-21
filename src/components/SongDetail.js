b931b21edad34f6e3692915915aee42018c23a94
Arianna Choza <unachoza@gmail.com>
2019-03-21 13:12:48 -0400

    song details displaying

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-const SongDetail = () => {
-    return <div>Song Detail</div>
+const SongDetail = ({ song }) => {
+    if (!song) {
+        return <div>Select a Song</div>
+    }
+    return (
+        <div>
+            <h3>Details for: </h3>
+            <p>
+                Title: {song.title} <br />
+                Duration: {song.duration}
+            </p>
+        </div>
+    )
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
+    console.log(state)
    return  {song: state.selectedSong}
 }
 
