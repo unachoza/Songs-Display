a4c9e0123c9089182a4f5d591d8f7bf319fdc345
Arianna Choza <unachoza@gmail.com>
2019-03-21 14:11:54 -0400

    fixing margins

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import SongDetail from './SongDetail'
 const App = () => {
     return (
         <div>
-           <h1 style={{marginLeft: "35%", marginBottom: "4%"}}> See Details of Hit 20 th Century Songs </h1> 
+           <h1 style={{marginLeft: "32%", marginBottom: "4%"}}> See Details of Hit 20 th Century Songs </h1> 
             <div className = "ui grid " style={{marginLeft: "15%" }}>
                 <div className="ui row">
                     <div className="column eight wide">
